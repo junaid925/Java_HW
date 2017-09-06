974a25ec571d922ab7a4af3380ac1a04c285382d
junaid925 <junaid.nawaz92@hotmail.com>
2017-09-06 05:47:41 -0400

    Add files via upload

---
+++ MainQ4.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+
+/**
+ *
+ * @author junaid
+ */
+public class MainQ4 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNext()) {
+                     System.out.println(scanner.next());
+    }
+    
+} }
